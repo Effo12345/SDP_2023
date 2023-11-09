7640dcc32a30252ea56fdc916ff42a0d93f2400a
Effo12345 <4p7septipuses@gmail.com>
2023-11-09 00:20:01 -0500

    Initial Template Commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "include/main.hpp"
+#include "FEHLCD.h"
+
+int main()
+{
+    LCD.WriteLine("Hello World!");
+    while (1) {
+        LCD.Update();
+        // Never end
+    }
+    return 0;
+}
