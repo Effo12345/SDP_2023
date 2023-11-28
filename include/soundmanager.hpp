e2a428000e15e8f0afc04d2619a6d78b6af0c843
Effo12345 <4p7septipuses@gmail.com>
2023-11-28 15:57:29 -0500

    Removed extranneous music functions

--- include/soundmanager.hpp
+++ include/soundmanager.hpp
@@ -34,7 +34,5 @@ private:
 public:
     SoundManager();
     int play(std::string path, float volume = 1, bool ShouldLoop = false); // plays the audio file with specified volume and can be looped
-    void playMusic(std::string path, bool doLoop = true);
-    void stopMusic();
     std::string BasePath; // Directory where all audio files (relevant to project) are stored i.e if all audio files are stored in "D:\game" than set BasePath to "D:\game", this will be automatically added in path of every audio file
 };
