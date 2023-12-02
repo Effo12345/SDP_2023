eece4c7a72bf53da62afc44aa68bd19693c1e966
Effo12345 <4p7septipuses@gmail.com>
2023-12-01 22:03:23 -0500

    Cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -4,67 +4,6 @@
 
 Stats stats;
 
-/*
-FEHIcon::Icon btns[5];
-char labels[][20] = {"Play", "Instructions", "Stats", "Credits"};
-
-FEHIcon::Icon back[1];
-char backLabel[1][20] = {"Back"};
-
-std::string backgroundFileNames[] = {"images/backgrounds/credits.pic", "images/backgrounds/instructions.pic", "images/backgrounds/stats.pic"};
-
-void drawBackground (std::string backgroundFileName) {
-
-    FEHImage backgroundImage;
-
-    backgroundImage.Open(backgroundFileName.c_str());
-
-    backgroundImage.Draw(0, 0);
-
-    backgroundImage.Close();
-
-}
-
-void drawBackBtn() {
-    FEHIcon::DrawIconArray(back, 1, 1, 0, 210, 0, 270, backLabel, WHITE, WHITE);
-}
-
-void menu() {
-    FEHIcon::DrawIconArray(btns, 2, 2, 50, 50, 10, 10, labels, WHITE, WHITE);
-}
-
-void play() {
-    LCD.WriteAt("Play game", 100, 100);
-    drawBackBtn();
-}
-
-void instructions() {
-
-    drawBackground(backgroundFileNames[1]);
-
-    drawBackBtn();
-}
-
-void stats() {
-
-    drawBackground(backgroundFileNames[2]);
-
-    LCD.SetFontColor(WHITE);
-    LCD.WriteAt("23", 235, 85);
-    LCD.WriteAt("2", 235, 105);
-    LCD.WriteAt("3", 235, 125);
-
-    drawBackBtn();
-}
-
-void credits() {
-
-    drawBackground(backgroundFileNames[0]);
-
-    drawBackBtn();
-}
-*/
-
 void drawBackground(std::string path) {
     FEHImage backgroundImage;
 
@@ -133,11 +72,6 @@ void playGame() {
 
 
 int main() {
-
-//    GameManager level1("images/backgrounds/1.pic", "level1.wav", 30, 10, 30, 50);
-//    level1.initialize();
-
-    
     stats.readStats();
 
     std::array<std::function<void()>, 4> buttonActions {{
@@ -204,45 +138,6 @@ int main() {
 
         Sleep(50);
     }
-    
-
-    /*
-    std::array<std::function<void()>, 5> screens {{
-        menu, play, instructions, stats, credits
-    }};
-
-    int btnMode = 0;  // Non-toggle mode
-    int renderTarget = 0;
-    bool isMainMenu = true;
-
-
-    while(1) {
-        int xTouch, yTouch;
-
-        if(LCD.Touch(&xTouch, &yTouch)) {
-            if(!isMainMenu) {
-                if(back[0].Pressed(xTouch, yTouch, btnMode)) {
-                    renderTarget = 0;
-                    isMainMenu = true;
-                }
-                continue;
-            }
-
-            for(int i = 0; i < 5; i++) {
-                if(btns[i].Pressed(xTouch, yTouch, btnMode)) {
-                    renderTarget = i + 1;
-                    isMainMenu = false;
-                    break;
-                }
-            }
-        }
-
-        LCD.Clear();
-        screens[renderTarget]();
-
-        Sleep(100);
-    }
-    */
 
     return 0;
 }
